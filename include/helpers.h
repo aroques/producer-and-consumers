daee2cb28c5f5a6712f5677fbdaac97c3dac1094
Alec Roques <alec.roques@gmail.com>
2018-02-19 20:25:42 -0600

    Add include directory

---
+++ include/helpers.h
@@ -0,0 +1,9 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+char** split_string(char* str, char* delimeter);
+void sigint_handler(int s);
+void add_signal_handler();
+void sigint_handler(int s);
+
+#endif
