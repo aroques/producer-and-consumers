10b6a05e467841077ab97ef420163496c2982717
Alec Roques <alec.roques@gmail.com>
2018-05-05 15:26:06 -0500

    Cleanup consumer code

--- consumer.c
+++ consumer.c
@@ -10,33 +10,22 @@
 
 void add_signal_handler();
 void handle_sigterm(int s);
+void open_logfile();
 
 struct SharedMemory* shmem;
 FILE* log_fp;
 
 int main (int argc, char *argv[]) {
     int j, sleep_time;
+    char buff[100];
     srand(time(0) ^ getpid());
-    int i;
-    for (i = 0; i < 5; i++) {
-        //printf("argv[%d] = %s\n", i, argv[i]);
-    }
 
-    //printf("\n");
-    i = atoi(argv[1]);
-    //printf("i = %d\n", i);
+    int i = atoi(argv[1]);
+
     const int NUM_PROC = atoi(argv[2]);
 
-//    char filename[50];
-//    sprintf(filename, "./consumer%d.log", i);
-//    log_fp = fopen(filename, "w");
-//    if (log_fp == NULL) {
-//        perror("failed to open data.txt for reading\n");
-//        exit(1);
-//    }
-//    fprintf(log_fp, "%s Started\n", get_timestamp());
-
-    printf("Consumer %2d: %s Started\n", i, get_timestamp());
+    sprintf(buff, "Consumer %2d: %s Started\n", i, get_timestamp());
+    print_and_write(buff, log_fp);
 
     add_signal_handler();
 
@@ -45,24 +34,18 @@ int main (int argc, char *argv[]) {
     struct SharedMemoryIDs* shmem_ids = get_shared_memory_ids(ids);
     shmem = attach_consumer_shared_memory(shmem_ids);
 
+    // Unpack shared memory pointers
     int* buffer_flag = shmem->buffer_flag;
-    //char* buffer = shmem-> buffer;
-    int* turn = shmem->turn;
-    int* flag = shmem->flag; /* Flag corresponding to each process in shared memory */
-
-    //printf("flag[%d] %i\n", flag[i], i);
+    char* buffer = shmem-> buffer;
+	int* turn = shmem->turn;
+	int* flag = shmem->flag; /* Flag corresponding to each process in shared memory */
 
     do {
-
 
         do {
             flag[i] = want_in; // Raise my flag
 
-            //printf("turn = %d\n", *turn);
-
             j = *turn; // Set local variable
-
-            //printf("turn = %d\n", *turn);
 
             // wait until its my turn
             while ( j != i ) {
@@ -73,8 +56,9 @@ int main (int argc, char *argv[]) {
             flag[i] = in_cs;
 
             // Check that no one else is in critical section
-            //fprintf(log_fp, "%s Check\n", get_timestamp());
-            printf("Consumer: %s Checked\n", get_timestamp());
+            sprintf(buff, "Consumer: %s Checked\n", get_timestamp());
+            print_and_write(buff, log_fp);
+
             for (j = 0; j < NUM_PROC; j++)
                 if ((j != i) && (flag[j] == in_cs))
                     break;
@@ -86,7 +70,9 @@ int main (int argc, char *argv[]) {
         for (i = 0; i < NUM_BUFFERS; i++) {
             if (buffer_flag[i] == 1) {
                 // Buffer is full so read it
-                printf("Consumer: %s Read \t %d \t Message\n", get_timestamp(), i);
+                sprintf(buff, "Consumer: %s Read \t %d \t Message\n", get_timestamp(), i);
+                print_and_write(buff, log_fp);
+                
                 buffer_flag[i] = 0;
                 break;
             }
@@ -103,8 +89,10 @@ int main (int argc, char *argv[]) {
 
         // Remainder section
         sleep_time = (rand() % 5) + 1;
-        fprintf(log_fp, "%s Sleep \t %d\n", get_timestamp(), sleep_time);
-        printf("Consumer: %s Sleep \t %d\n", get_timestamp(), sleep_time);
+
+        sprintf(buff, "Consumer: %s Sleep \t %d\n", get_timestamp(), sleep_time);
+        print_and_write(buff, log_fp);
+
         sleep(sleep_time);
 
         } while (1);
@@ -124,4 +112,14 @@ void add_signal_handlers() {
 void handle_sigterm(int sig) {
     detach_shared_memory(shmem);
     _exit(0);
+}
+
+void open_logfile() {
+    char filename[50];
+    sprintf(filename, "./consumer%d.log", i);
+    log_fp = fopen(filename, "w");
+    if (log_fp == NULL) {
+        perror("failed to open %s for writing\n", filename);
+        exit(1);
+    }
 }
